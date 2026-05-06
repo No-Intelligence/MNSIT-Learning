9368ab0ad292460fc063bf5226a318f9b13828c6
No-Intelligence <taiyaki.0gtyan@gmail.com>
2026-05-06 21:25:19 +0900

    add prototype

--- mllib.h
+++ mllib.h
@@ -16,4 +16,16 @@ typedef struct {
     parameter_t *parameters;
 } neural_network_t;
 
+layer_t* alloc_layer (int n_layer_units);
+
+void free_layer (layer_t *p, int n_layer_units);
+
+parameter_t* alloc_parameter (int n_weight, int n_bias);
+
+void free_parameter (parameter_t *p, int n_weight, int n_bias);
+
+neural_network_t* alloc_neural_network (int n_layers, int *layer_units);
+
+void free_neural_network (neural_network_t *p, int n_layers, int *layer_units);
+
 #endif
